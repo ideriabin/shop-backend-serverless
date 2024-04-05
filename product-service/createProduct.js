45acfd9df16d93ac7d4a0064bad9b9c6d603de41
Igor Deriabin <igor@deriab.in>
2024-04-05 15:54:57 +0300

    Add basic eror validation for createProduct

--- product-service/createProduct.js
+++ product-service/createProduct.js
@@ -1,10 +1,13 @@
 const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
 const { marshall } = require('@aws-sdk/util-dynamodb');
 const uuid = require('uuid');
+const { ValidationError } = require('./helpers');
 
 const client = new DynamoDBClient();
 
 module.exports = async ({ title, description, price, count = 0 }) => {
+  if (!title || isNaN(Number(price)) || Number(price) <= 0) throw new ValidationError('Invalid product data!');
+
   const id = uuid.v4();
 
   await client.send(new PutItemCommand({
--- product-service/helpers.js
+++ product-service/helpers.js
@@ -1,7 +1,11 @@
+class ValidationError extends Error {}
+
+exports.ValidationError = ValidationError;
+
 exports.executeWithErrorHandler = async (fn) => {
   try {
     return { statusCode: 200, body: JSON.stringify(await fn()) };
   } catch (e) {
-    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
+    return { statusCode: e instanceof ValidationError ? 400 : 500, body: JSON.stringify({ error: e.message }) };
   }
 };
