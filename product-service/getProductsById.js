56fcd0ca8ac00cf6a618fcca996510f5626430eb
Igor Deriabin <igor@deriab.in>
2024-04-05 15:22:16 +0300

    Integrate lambdas with DynamoDB

--- product-service/getProductsById.js
+++ product-service/getProductsById.js
@@ -1,7 +1,15 @@
-const { products } = require('./mocks');
+const { DynamoDBClient, GetItemCommand } = require('@aws-sdk/client-dynamodb');
+const { unmarshall } = require('@aws-sdk/util-dynamodb');
 
-module.exports = (productId) => {
-  const product = products.find(p => p.id === productId);
-  if (!product) throw new Error('Product not found');
-  return Promise.resolve(product);
+const client = new DynamoDBClient();
+
+module.exports = async (productId) => {
+  const product = await client.send(
+    new GetItemCommand({ TableName: process.env.PRODUCTS_TABLE, Key: { id: { S: productId } } })
+  );
+  const stock = await client.send(
+    new GetItemCommand({ TableName: process.env.STOCK_TABLE, Key: { product_id: { S: productId } } })
+  );
+  if (!product.Item || !stock.Item) throw new Error('Product not found');
+  return Promise.resolve({ ...unmarshall(product.Item), count: stock.Item.count.N });
 };
